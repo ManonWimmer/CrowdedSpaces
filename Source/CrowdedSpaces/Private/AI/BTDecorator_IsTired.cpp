a766c6b06eb18614f89f0af1b4ac669f24719c66
Manon Wimmer <manonwimmer30@gmail.com>
2026-01-25 21:52:26 +0100

    [ADD] BTDecorator IsTired check if npc energy < min for tired

---
+++ Source/CrowdedSpaces/Private/AI/BTDecorator_IsTired.cpp
@@ -0,0 +1,26 @@
+﻿#include "AI/BTDecorator_IsTired.h"
+
+#include "AIController.h"
+#include "Resources/EnergyComponent.h"
+
+UBTDecorator_IsTired::UBTDecorator_IsTired(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = "Is Tired";
+}
+
+bool UBTDecorator_IsTired::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController)
+		return false;
+
+	APawn* Pawn = AIController->GetPawn();
+	if (!Pawn)
+		return false;
+
+	UEnergyComponent* EnergyComp = Pawn->FindComponentByClass<UEnergyComponent>();
+	if (!EnergyComp)
+		return false;
+	
+	return EnergyComp->GetEnergy() <= TiredUnderEnergy;
+}
---
+++ Source/CrowdedSpaces/Public/AI/BTDecorator_IsTired.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_BlackboardBase.h"
+#include "BTDecorator_IsTired.generated.h"
+
+UCLASS()
+class CROWDEDSPACES_API UBTDecorator_IsTired : public UBTDecorator_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	explicit UBTDecorator_IsTired(FObjectInitializer const& ObjectInitializer);
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+	UPROPERTY(EditAnywhere, Category="Condition")
+	int32 TiredUnderEnergy = 30;
+};
+
