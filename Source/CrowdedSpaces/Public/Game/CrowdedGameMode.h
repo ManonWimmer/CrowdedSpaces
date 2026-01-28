7e9ec544407533886a1e4b0f6f1262ee19a1b167
Manon Wimmer <manonwimmer30@gmail.com>
2026-01-28 16:27:09 +0100

    [FIX] SetGameMode_Building & SetGameMode_Game to use in UI without variable game state reset

--- Source/CrowdedSpaces/Public/Game/CrowdedGameMode.h
+++ Source/CrowdedSpaces/Public/Game/CrowdedGameMode.h
@@ -22,6 +22,12 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "GameMode")
 	void SetGameMode(EGameModeState NewGameMode);
 
+	UFUNCTION(BlueprintCallable, Category = "GameMode")
+	void SetGameMode_Game() { SetGameMode(EGameModeState::Game); }
+
+	UFUNCTION(BlueprintCallable, Category = "GameMode")
+	void SetGameMode_Building() { SetGameMode(EGameModeState::Building); }
+
 	UPROPERTY(BlueprintAssignable)
 	FOnGameModeChanged OnGameModeChanged;
 
