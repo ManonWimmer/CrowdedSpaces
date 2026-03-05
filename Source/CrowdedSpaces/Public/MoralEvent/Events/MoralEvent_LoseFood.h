127e63fc27166cdeaae68cba3e5d1953e408e276
Manon Wimmer <manonwimmer30@gmail.com>
2026-03-05 22:45:10 +0100

    [ADD] Moral events lose money & lose food

---
+++ Source/CrowdedSpaces/Public/MoralEvent/Events/MoralEvent_LoseFood.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MoralEvent/MoralEvent.h"
+#include "MoralEvent_LoseFood.generated.h"
+
+UCLASS()
+class CROWDEDSPACES_API UMoralEvent_LoseFood : public UMoralEvent
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ClickOnChoice(EMoralEventType Choice) override;
+
+	virtual void SetupChoices() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float FoodToLose = 100;
+};
