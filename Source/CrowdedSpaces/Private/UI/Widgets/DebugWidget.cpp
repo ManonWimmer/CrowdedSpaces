c051c7f82b2a50cbd287fcc5de99a677330c75ba
Manon Wimmer <manonwimmer30@gmail.com>
2026-03-20 17:18:29 +0100

    [ADD] Debug add money / food / electricity + UI

--- Source/CrowdedSpaces/Private/UI/Widgets/DebugWidget.cpp
+++ Source/CrowdedSpaces/Private/UI/Widgets/DebugWidget.cpp
@@ -1,8 +1,22 @@
 ﻿#include "UI/Widgets/DebugWidget.h"
 
+#include "Game/CrowdedGameState.h"
+
 void UDebugWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	UWorld* World = GetWorld();
+	if (!World)
+		return;
+
+	const TObjectPtr<ACrowdedGameState> GameState = World->GetGameState<ACrowdedGameState>();
+	if (!GameState)
+		return;
+
+	MoneyComponent = GameState->GetResourceComponent<EResourceType::Money>();
+	ElectricityComponent = GameState->GetResourceComponent<EResourceType::Electricity>();
+	FoodComponent = GameState->GetResourceComponent<EResourceType::Food>();
 }
 
 void UDebugWidget::Reset_Implementation()
@@ -18,6 +32,30 @@ void UDebugWidget::Setup_Implementation(AActor* SelectableActor)
 void UDebugWidget::Unsetup_Implementation()
 {
 	Super::Unsetup_Implementation();
+}
+
+void UDebugWidget::AddMoney(const int Amount)
+{
+	if (!MoneyComponent)
+		return;
+
+	MoneyComponent->AddResource(Amount);
+}
+
+void UDebugWidget::AddElectricity(const int Amount)
+{
+	if (!ElectricityComponent)
+		return;
+
+	ElectricityComponent->AddResource(Amount);
+}
+
+void UDebugWidget::AddFood(const int Amount)
+{
+	if (!FoodComponent)
+		return;
+
+	FoodComponent->AddResource(Amount);
 }
 
 void UDebugWidget::Init_Implementation()
