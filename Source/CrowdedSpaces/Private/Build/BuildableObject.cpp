0da6997b82af4aafa10f280a44e560bb06de6c75
Manon Wimmer <manonwimmer30@gmail.com>
2026-04-02 00:11:46 +0200

    [REWORK] CrowdedSpacesLog debug script

--- Source/CrowdedSpaces/Private/Build/BuildableObject.cpp
+++ Source/CrowdedSpaces/Private/Build/BuildableObject.cpp
@@ -8,8 +8,7 @@
 #include "Game/CrowdedGameState.h"
 #include "Player/PlayerHelpers.h"
 #include "UI/GameHUD.h"
-
-#define BO_LOG(Format, ...) UE_LOG(LogTemp, Warning, TEXT("[BuildableObject:%s] " Format), *GetNameSafe(this), ##__VA_ARGS__)
+#include "Debug/CrowdedSpacesLogs.h"
 
 ABuildableObject::ABuildableObject()
 {
@@ -118,7 +117,7 @@ void ABuildableObject::CheckCantBeUsedStopTask() const
 		{
 			if (CurrentTask)
 			{
-				BO_LOG("Force stopping task for NPC: %s", *GetNameSafe(UsingNPC.Get()));
+				CS_LOG("Force stopping task for NPC: %s", *GetNameSafe(UsingNPC.Get()));
 				CurrentTask->ForceStopTask();
 			}
 		}
@@ -141,25 +140,25 @@ void ABuildableObject::SetIsActivated(const bool bActivated)
 
 bool ABuildableObject::TryReserve(ANPC* NPC)
 {
-	BO_LOG("TryReserve by NPC: %s | Current ComingNPC: %s",
+	CS_LOG("TryReserve by NPC: %s | Current ComingNPC: %s",
 		*GetNameSafe(NPC),
 		*GetNameSafe(ComingNPC.Get()));
 
 	if (bHasNPCUsing)
 	{
-		BO_LOG("RESERVE FAILED: has npc using");
+		CS_LOG("RESERVE FAILED: has npc using");
 		return false;
 	}
 
 	if (ComingNPC.IsValid() && ComingNPC != NPC)
 	{
-		BO_LOG("RESERVE FAILED: already reserved by other NPC");
+		CS_LOG("RESERVE FAILED: already reserved by other NPC");
 		return false;
 	}
 
 	ComingNPC = NPC;
 	bHasNPCComing = true;
-	BO_LOG("RESERVED SUCCESS by NPC: %s", *GetNameSafe(NPC));
+	CS_LOG("RESERVED SUCCESS by NPC: %s", *GetNameSafe(NPC));
 	
 	OnNPCComingChanged.Broadcast(bHasNPCComing);
 	return true;
@@ -185,14 +184,14 @@ void ABuildableObject::Release(ANPC* NPC)
 
 void ABuildableObject::StartUsing(ANPC* NPC)
 {
-	BO_LOG("StartUsing attempt NPC: %s | ComingNPC: %s | UsingNPC: %s",
+	CS_LOG("StartUsing attempt NPC: %s | ComingNPC: %s | UsingNPC: %s",
 		*GetNameSafe(NPC),
 		*GetNameSafe(ComingNPC.Get()),
 		*GetNameSafe(UsingNPC.Get()));
 	
 	if (ComingNPC != NPC)
 	{
-		BO_LOG("START USING FAILED: ComingNPC mismatch");
+		CS_LOG("START USING FAILED: ComingNPC mismatch");
 		return;
 	}
 	
@@ -202,7 +201,7 @@ void ABuildableObject::StartUsing(ANPC* NPC)
 	bHasNPCComing = false;
 	bHasNPCUsing = true;
 
-	BO_LOG("START USING SUCCESS NPC: %s", *GetNameSafe(NPC));
+	CS_LOG("START USING SUCCESS NPC: %s", *GetNameSafe(NPC));
 	
 	OnNPCComingChanged.Broadcast(bHasNPCComing);
 	OnNPCUsingChanged.Broadcast(bHasNPCUsing);
@@ -210,13 +209,13 @@ void ABuildableObject::StartUsing(ANPC* NPC)
 
 void ABuildableObject::StopUsing(ANPC* NPC)
 {
-	BO_LOG("StopUsing NPC: %s | Current UsingNPC: %s",
+	CS_LOG("StopUsing NPC: %s | Current UsingNPC: %s",
 		*GetNameSafe(NPC),
 		*GetNameSafe(UsingNPC.Get()));
 	
 	if (UsingNPC != NPC)
 	{
-		BO_LOG("STOP USING IGNORED (wrong NPC)");
+		CS_LOG("STOP USING IGNORED (wrong NPC)");
 		return;
 	}
 	
@@ -227,7 +226,7 @@ void ABuildableObject::StopUsing(ANPC* NPC)
 	ComingNPC = nullptr;
 	bHasNPCComing = false;
 	
-	BO_LOG("STOP USING SUCCESS");
+	CS_LOG("STOP USING SUCCESS");
 	
 	OnNPCUsingChanged.Broadcast(bHasNPCUsing);
 }
@@ -281,7 +280,7 @@ USlotComponent* ABuildableObject::GetFreeSlot()
 
 USlotComponent* ABuildableObject::ReserveSlot(ANPC* NPC)
 {
-	BO_LOG("ReserveSlot  NPC: %s", *GetNameSafe(NPC));
+	CS_LOG("ReserveSlot  NPC: %s", *GetNameSafe(NPC));
 	
 	for (USlotComponent* Slot : Slots)
 	{
@@ -289,7 +288,7 @@ USlotComponent* ABuildableObject::ReserveSlot(ANPC* NPC)
 		{
 			Slot->SetOccupied(true, NPC);
 			
-			BO_LOG("Slot reserved: %s by NPC: %s",
+			CS_LOG("Slot reserved: %s by NPC: %s",
 				*Slot->GetName(),
 				*GetNameSafe(NPC));
 			
@@ -297,26 +296,26 @@ USlotComponent* ABuildableObject::ReserveSlot(ANPC* NPC)
 		}
 	}
 
-	BO_LOG("ReserveSlot FAILED: no free slot");
+	CS_LOG("ReserveSlot FAILED: no free slot");
 	
 	return nullptr;
 }
 
 void  ABuildableObject::ReleaseSlot(ANPC* NPC)
 {
-	BO_LOG("ReleaseSlot NPC: %s", *GetNameSafe(NPC));
+	CS_LOG("ReleaseSlot NPC: %s", *GetNameSafe(NPC));
 	
 	for (USlotComponent* Slot : Slots)
 	{
 		if (Slot && Slot->OccupyingNPC == NPC)
 		{
-			BO_LOG("Slot released: %s", *Slot->GetName());
+			CS_LOG("Slot released: %s", *Slot->GetName());
 			Slot->SetOccupied(false, nullptr);
 			return;
 		}
 	}
 	
-	BO_LOG("WARNING: No slot found for NPC");
+	CS_LOG_WARNING("WARNING: No slot found for NPC");
 }
 
 
