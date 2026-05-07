0fa9cb939858a701da3d4a168412c0a0aa7da0c8
Manon Wimmer <manonwimmer30@gmail.com>
2026-05-07 13:59:53 +0200

    [ADD] Mixamo character (gave up retarget) + animation blueprint with idle/walking blend

--- Source/CrowdedSpaces/Private/AI/BTTasks/BTTask_FindRandomLocation.cpp
+++ Source/CrowdedSpaces/Private/AI/BTTasks/BTTask_FindRandomLocation.cpp
@@ -3,6 +3,7 @@
 #include "NavigationSystem.h"
 #include "AI/NPCController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Debug/CrowdedSpacesLogs.h"
 
 UBTTask_FindRandomLocation::UBTTask_FindRandomLocation(FObjectInitializer const& ObjectInitializer)
 {
@@ -16,25 +17,40 @@ EBTNodeResult::Type UBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeCompone
 {
 	TObjectPtr<ANPCController> const Controller = Cast<ANPCController>(OwnerComp.GetAIOwner());
 	if (!Controller)
+	{
+		CS_LOG("Cant find controller");
 		return EBTNodeResult::Failed;
+	}
 
 	NPC = Cast<ANPC>(Controller->GetPawn());
 	if (!NPC)
+	{
+		CS_LOG("Cant find npc");
 		return EBTNodeResult::Failed;
+	}
 	
 	FVector const Origin = NPC->GetActorLocation();
 	TObjectPtr<UNavigationSystemV1> const NavigationSystem = UNavigationSystemV1::GetCurrent(GetWorld());
 	if (!NavigationSystem)
+	{
+		CS_LOG("Cant find nav system");
 		return EBTNodeResult::Failed;
+	}
 	
 	FNavLocation Location;
 	bool bFoundPoint = NavigationSystem->GetRandomPointInNavigableRadius(Origin, SearchRadius, Location);
 	if (!bFoundPoint)
+	{
+		CS_LOG("Cant find point in nav mesh");
 		return EBTNodeResult::Failed;
+	}
 
 	const TObjectPtr<UBlackboardComponent> Blackboard = OwnerComp.GetBlackboardComponent();
 	if (!Blackboard)
+	{
+		CS_LOG("Cant find blackboard");
 		return EBTNodeResult::Failed;
+	}
 	
 	Blackboard->SetValueAsVector(RandomLocationKey.SelectedKeyName, Location);
 	
